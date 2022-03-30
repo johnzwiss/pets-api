b98b468e76433968cbff8baa3a6d25eb4c3e709a
johnzwiss <johnzwiss@gmail.com>
2022-03-30 11:50:43 -0400

    all routes working

--- app/routes/pet_routes.js
+++ app/routes/pet_routes.js
@@ -12,6 +12,7 @@ const requireOwnership = customErrors.requireOwnership
 const requireToken = passport.authenticate('bearer', { session: false })
 
 const removeBlanks = require('../../lib/remove_blank_fields')
+const pet = require('../models/pet')
 
 const router = express.Router() 
 
@@ -65,6 +66,46 @@ router.post('/pets', requireToken, (req, res, next) => {
 		.catch(next)
 })
 
+
+// UPDATE
+// PATCH /examples/5a7db6c74d55bc51bdf39793
+router.patch('/pets/:id', requireToken, removeBlanks, (req, res, next) => {
+	// if the client attempts to change the `owner` property by including a new
+	// owner, prevent that by deleting that key/value pair
+	delete req.body.pet.owner
+
+	Pet.findById(req.params.id)
+		.then(handle404)
+		.then((pet) => {
+			// pass the `req` object and the Mongoose record to `requireOwnership`
+			// it will throw an error if the current user isn't the owner
+			requireOwnership(req, pet)
+
+			// pass the result of Mongoose's `.update` to the next `.then`
+			return pet.updateOne(req.body.pet)
+		})
+		// if that succeeded, return 204 and no JSON
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
+
+// DESTROY
+// DELETE /examples/5a7db6c74d55bc51bdf39793
+router.delete('/pets/:id', requireToken, (req, res, next) => {
+	pet.findById(req.params.id)
+		.then(handle404)
+		.then((pet) => {
+			// throw an error if current user doesn't own `example`
+			requireOwnership(req, pet)
+			// delete the example ONLY IF the above didn't throw
+			pet.deleteOne()
+		})
+		// send back 204 and no content if the deletion succeeded
+		.then(() => res.sendStatus(204))
+		// if an error occurs, pass it to the handler
+		.catch(next)
+})
 //replace with routes plz 
 
 
